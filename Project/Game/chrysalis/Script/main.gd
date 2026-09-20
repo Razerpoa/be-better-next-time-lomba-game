35d96fff43f400154a469039be932d70f73d74f8
Han <subhansihabudin2@gmail.com>
2026-09-20 12:08:51 +0700

    Masukin Texture than

--- Project/Game/chrysalis/Script/main.gd
+++ Project/Game/chrysalis/Script/main.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var MainUi = $MainMenuManeger
 var mainVisible: bool
 @onready var MainNode = $Playing
+@onready var player = $Playing/Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +13,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	MainNode.visible = MainUi.mainVisible
+	if player.position.y <= -5:
+		player.position = Vector3(0, 3, 0)
 	pass
