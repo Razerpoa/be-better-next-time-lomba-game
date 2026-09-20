e9c408f1bc56096363284cc55edb79c6a069c404
Han <subhansihabudin2@gmail.com>
2026-09-20 21:08:00 +0700

    Good Night All

--- Project/Game/chrysalis/Script/npc.gd
+++ Project/Game/chrysalis/Script/npc.gd
@@ -1,1 +1,8 @@
 extends CharacterBody3D
+
+func _physics_process(delta: float) -> void:
+	var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	move_and_slide()
+	pass
